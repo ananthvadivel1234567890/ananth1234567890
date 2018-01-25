5c8020656d2dcb6eaeb2434a265d483bfdf68bdc
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-01-25 17:02:26 +0530

    Create po.c

---
+++ po.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	void main()
+	{
+	    int a;
+	    printf("Enter the number");
+	    scanf("%d",&a);
+	    if (a%2==0)
+	    {
+	        printf("The given number is EVEN");
+	    }
+	    else
+	    {
+	        printf("the given number is ODD");
+	    }
+	    getch();
+	}
+	
